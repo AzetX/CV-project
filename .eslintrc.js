19845d91fede2f52f8942e4ebdb7f09c5a9b6900
Nikita Solovev <n.solovev@andersenlab.com>
2021-09-06 15:25:15 +0300

    Branch/Master - initial project configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,77 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+    root: true,
+    env: {
+        node: true,
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        'plugin:jsdoc/recommended'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'semi': 2,
+        'comma-dangle': ['error', {
+            'objects': 'always',
+        }],
+        'quotes': ['error', 'single', { 'allowTemplateLiterals': true, }],
+
+        'valid-jsdoc': [2, {
+            'preferType': {
+                'String': 'string',
+                'Number': 'number',
+                'Boolean': 'boolean',
+                'object': 'Object',
+                'array': 'Array',
+            },
+            'prefer': {
+                'return': 'returns',
+            },
+            'requireReturn': false,
+            'requireReturnDescription': false,
+        }],
+        'jsdoc/require-returns-description': 0,
+        'jsdoc/require-returns-check': 0,
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true,
+            },
+        },
+        {
+            files: [
+                '*.spec.js'
+            ],
+            rules: {
+                'require-jsdoc-except/require-jsdoc': 'off',
+                'jsdoc/check-tag-names': 'off',
+            },
+        },
+        {
+            files: [
+                'tests/utils.js'
+            ],
+            rules: {
+                'jsdoc/no-undefined-types': 'off',
+            },
+        },
+        {
+            files: [
+                'resources/**/*.stories.js'
+            ],
+            rules: {
+                'valid-jsdoc': 'off',
+                'require-jsdoc-except/require-jsdoc': 'off',
+            },
+        }
+    ],
+};
